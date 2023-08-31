6334b0420744687c6af83574389153a0a6c112b3
robin schubert <robschu123@hotmail.de>
2023-08-31 10:16:12 +0200

    add the toggle for the bookmark and showanswer

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// phew… not a lot going on here. Please add some code!
+const bookMarkToggle = document.querySelector('[data-js="bookMark"]');
+
+bookMarkToggle.addEventListener("click", () => {
+bookMarkToggle.classList.toggle("bookmark--active");
+});
+
+const card =document.querySelector('[class="card-list__item"]');
+
+const answerButton =document.querySelector('[class="card__button-answer"]');
+
+const shownAnswer =document.querySelector('[data-js="answerOne"]');
+
+answerButton.addEventListener("click", () => {
+    shownAnswer.classList.toggle("card__answer");
+    if(answerButton.innerHTML ==="Hide answer"){
+        answerButton.innerHTML ="Show answer"
+    } else {
+        answerButton.innerHTML ="Hide answer"
+    }
+});
+
+
